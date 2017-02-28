dd71932d9f0ac8322f3f07cf36051e1c94e00813
xmanatee <xmanatee.one@gmail.com>
2017-02-28 03:38:11 +0300

    solving travis issues : long line issue

--- src/twitter/preprocessing.py
+++ src/twitter/preprocessing.py
@@ -7,7 +7,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class TextCleaner(BaseEstimator, TransformerMixin):
     _numeric = re.compile("(\$)?\d+([\.,]\d+)*")
     _links = re.compile(
-        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|" + \
+        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|"
         "[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
     _trans_table = str.maketrans({key: None for key in string.punctuation})
 
