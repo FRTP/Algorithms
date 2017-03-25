d35e255fbc7b6fe7be4d23e852207edfbd07111f
Anton Karazeev <anton.karazeev@gmail.com>
2017-03-25 19:05:12 +0300

    Initial commit. Add main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def featureNormalize():
+
+def main():
+
+    clear
+    load('retDAX.txt')
+    load('DAX.txt')
+
+    M = 10
+    T = 500  # The number of time series inputs to the trader
+    N = 100
+
+    return
+
+
+if __name__ == '__main__':
+    main()
